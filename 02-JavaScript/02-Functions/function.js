de669206d1331061f5183c5568d620bc55b0c1c8
LadaKashubo <lkashubo@yandex.ru>
2025-07-16 23:08:48 +0300

    Choose img DONE

--- 02-JavaScript/02-Functions/function.js
+++ 02-JavaScript/02-Functions/function.js
@@ -12,9 +12,9 @@ function Power(base, exponent)
 function SwitchBackground()
 {
     let switchButton = document.getElementById('switchBackground');
-    console.log(switchButton.attributes.src);
+    //console.log(switchButton.attributes.src);
     //switchButton.attributes.src.nodeValue = switchButton.attributes.src.nodeValue == 'img/moon.png' ? 'img/sun.png' : 'img/moon.png'; 
-    if (switchButton.attributes.src.nodeValue == 'img/moon.png')
+    /*if (switchButton.attributes.src.nodeValue == 'img/moon.png')
     {
         switchButton.attributes.src.nodeValue = 'img/sun.png';
         document.body.style.background = "linear-gradient(to bottom left, #240672, black, black)";
@@ -25,6 +25,29 @@ function SwitchBackground()
         switchButton.attributes.src.nodeValue = 'img/moon.png';
         document.body.style.background = "linear-gradient(to bottom left, #FFDD40, wheat, wheat)";
         document.body.style.color = "black";
+    }*/
+    let delay = Number(document.getElementById("delay").value);
+    console.log(delay);
+    document.body.style.transition = `background-color ${delay}s, color ${delay}s`;
+    document.getElementById('switchBackground').style.transition = `background-image ${delay}s`;
+    document.body.className = document.body.className == "light" ? "dark" : "light";
+}
+document.addEventListener("mousemove", function (event)
+{
+    let x = event.clientX;
+    let y = event.clientY;
+    document.getElementById("mouse").innerHTML = `X = ${x}, Y = ${y}`;
+});
+function setImage()
+{
+    let filename = document.getElementById('image-file');
+    //console.log(filename);
+    //let splitted_filename = filename.split('\\');
+    //document.getElementById("photo").imgElement.src = splitted_filename[splitted_filename.length - 1];
+    let reader = new FileReader();
+    reader.onload = function (e)
+    {
+        document.getElementById('photo').src = e.target.result;
     }
-
+    reader.readAsDataURL(filename.files[0]);
 }
