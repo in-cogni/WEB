10e7c3c20fbfc681871c43a2b61035319fb0e844
LadaKashubo <lkashubo@yandex.ru>
2025-07-23 20:26:33 +0300

    DOM configuration (append. prepend...)

---
+++ 02-JavaScript/03-DOM/DOM.js
@@ -0,0 +1,11 @@
+﻿// JavaScript source code
+let check = document.getElementById("check");
+check.innerHTML = "Hello DOM";
+console.log(check.outerHTML);
+
+let configuration = document.getElementById("configuration");
+//configuration.append("Этот текст добавляется в конец элемента (перед закрывающем дескриптором)");
+configuration.append("append();");
+configuration.prepend("prepend();");
+configuration.before("Добавление элементов к ноде");
+configuratiom.after("Усе");
